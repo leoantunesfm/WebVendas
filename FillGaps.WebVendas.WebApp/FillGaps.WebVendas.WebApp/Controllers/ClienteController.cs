fc7d88e03558f541f04cdbe653d46db5e1363c28
Leandro Machado <leoantunes.fm@gmail.com>
2025-05-29 00:25:18 -0300

    Criacao dos recursos do cadastro de clientes

---
+++ FillGaps.WebVendas.WebApp/FillGaps.WebVendas.WebApp/Controllers/ClienteController.cs
@@ -0,0 +1,91 @@
+﻿using FillGaps.WebVendas.WebApp.DAL;
+using FillGaps.WebVendas.WebApp.Entities;
+using FillGaps.WebVendas.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FillGaps.WebVendas.WebApp.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClienteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Cliente> clientes = _context.Cliente.ToList();
+            _context.Dispose();
+            return View(clientes);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            ClienteViewModel clienteViewModel = new ClienteViewModel();
+
+            if (id != null)
+            {
+                Cliente? cliente = _context.Cliente.Find(id);
+                if (cliente != null)
+                {
+                    clienteViewModel.IdCliente = (int)id;
+                    clienteViewModel.CodigoCliente = cliente.CodigoCliente;
+                    clienteViewModel.NomeCliente = cliente.NomeCliente;
+                    clienteViewModel.CPF_CNPJ = cliente.CPF_CNPJ;
+                    clienteViewModel.EmailCliente = cliente.EmailCliente;
+                    clienteViewModel.TelefoneCliente = cliente.TelefoneCliente;
+                }
+            }
+
+            return View(clienteViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(ClienteViewModel clienteViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Cliente oCliente = new Cliente
+                {
+                    IdCliente = clienteViewModel.IdCliente,
+                    CodigoCliente = clienteViewModel.CodigoCliente,
+                    NomeCliente = clienteViewModel.NomeCliente,
+                    CPF_CNPJ = clienteViewModel.CPF_CNPJ,
+                    EmailCliente = clienteViewModel.EmailCliente,
+                    TelefoneCliente = clienteViewModel.TelefoneCliente
+                };
+
+                if (oCliente.IdCliente == null)
+                {
+                    _context.Cliente.Add(oCliente);
+                }
+                else
+                {
+                    _context.Cliente.Update(oCliente);
+                }
+
+                _context.SaveChanges();
+            }
+            else
+            {
+                return View(clienteViewModel);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            Cliente? cliente = _context.Cliente.Find(id);
+            if (cliente != null)
+            {
+                _context.Cliente.Remove(cliente);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FillGaps.WebVendas.WebApp/FillGaps.WebVendas.WebApp/Models/ClienteViewModel.cs
@@ -0,0 +1,26 @@
+﻿using FillGaps.WebVendas.WebApp.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace FillGaps.WebVendas.WebApp.Models
+{
+    public class ClienteViewModel
+    {
+        public int? IdCliente { get; set; }
+
+        [Required(ErrorMessage = "O campo Código é obrigatório.")]
+        public string CodigoCliente { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string NomeCliente { get; set; }
+
+        [Required(ErrorMessage = "O campo CPF/CNPJ é obrigatório.")]
+        public string CPF_CNPJ { get; set; }
+
+        [Required(ErrorMessage = "O campo E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+        public string EmailCliente { get; set; }
+
+        [Required(ErrorMessage = "O campo Telefone é obrigatório.")]
+        public string TelefoneCliente { get; set; }
+    }
+}
