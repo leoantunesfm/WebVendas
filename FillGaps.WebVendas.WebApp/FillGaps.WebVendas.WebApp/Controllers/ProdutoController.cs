a63108e2c4f264f954ef100b0da2d77232971fc2
Leandro Machado <leoantunes.fm@gmail.com>
2025-05-29 23:18:50 -0300

    Criacao do cadastro de produtos

---
+++ FillGaps.WebVendas.WebApp/FillGaps.WebVendas.WebApp/Controllers/ProdutoController.cs
@@ -0,0 +1,116 @@
+﻿using FillGaps.WebVendas.WebApp.DAL;
+using FillGaps.WebVendas.WebApp.Entities;
+using FillGaps.WebVendas.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace FillGaps.WebVendas.WebApp.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProdutoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Produto> produtos = _context.Produto
+                .Include(c => c.Categoria)
+                .ToList();
+                                            
+            _context.Dispose();
+            return View(produtos);
+        }
+
+        private IEnumerable<SelectListItem> ListarCategorias()
+        {
+            List<SelectListItem> listaCategorias = new List<SelectListItem>();
+
+            listaCategorias.Add(new SelectListItem { Value = "0", Text = "Selecione uma categoria" });
+
+            foreach (var categoria in _context.Categoria.ToList())
+            {
+                listaCategorias.Add(new SelectListItem
+                {
+                    Value = categoria.IdCategoria.ToString(),
+                    Text = categoria.CodigoCategoria.ToString() + " - " + categoria.NomeCategoria.ToString()
+                });
+            }
+
+            return listaCategorias;
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            ProdutoViewModel produtoViewModel = new ProdutoViewModel();
+            produtoViewModel.ListaCategorias = ListarCategorias();
+
+            if (id != null)
+            {
+                Produto? produto = _context.Produto.Find(id);
+                if (produto != null)
+                {
+                    produtoViewModel.IdProduto = (int)id;
+                    produtoViewModel.CodigoProduto = produto.CodigoProduto;
+                    produtoViewModel.DescricaoProduto = produto.DescricaoProduto;
+                    produtoViewModel.QuantidadeProduto = produto.QuantidadeProduto;
+                    produtoViewModel.PrecoProduto = produto.PrecoProduto;
+                    produtoViewModel.IdCategoria = produto.IdCategoria;
+                }
+            }
+
+            return View(produtoViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Produto oProduto = new Produto
+                {
+                    IdProduto = produtoViewModel.IdProduto,
+                    CodigoProduto = produtoViewModel.CodigoProduto,
+                    DescricaoProduto = produtoViewModel.DescricaoProduto,
+                    QuantidadeProduto = (decimal)produtoViewModel.QuantidadeProduto,
+                    PrecoProduto = (decimal)produtoViewModel.PrecoProduto,
+                    IdCategoria = (int)produtoViewModel.IdCategoria
+                };
+
+                if (oProduto.IdProduto == null)
+                {
+                    _context.Produto.Add(oProduto);
+                }
+                else
+                {
+                    _context.Produto.Update(oProduto);
+                }
+
+                _context.SaveChanges();
+            }
+            else
+            {
+                produtoViewModel.ListaCategorias = ListarCategorias();
+                return View(produtoViewModel);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int id)
+        {
+            Produto? produto = _context.Produto.Find(id);
+            if (produto != null)
+            {
+                _context.Produto.Remove(produto);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FillGaps.WebVendas.WebApp/FillGaps.WebVendas.WebApp/Models/ProdutoViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+namespace FillGaps.WebVendas.WebApp.Models
+{
+    public class ProdutoViewModel
+    {
+        public int? IdProduto { get; set; }
+
+        [Required(ErrorMessage = "O campo Código é obrigatório.")]
+        public string CodigoProduto { get; set; }
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório.")]
+        public string DescricaoProduto { get; set; }
+
+        [Required(ErrorMessage = "O campo Quantidade é obrigatório.")]
+        [Range(0, double.MaxValue, ErrorMessage = "A quantidade deve ser um valor positivo.")]
+        public decimal? QuantidadeProduto { get; set; }
+
+        [Required(ErrorMessage = "O campo Preço é obrigatório.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço deve ser um valor positivo.")]
+        public decimal? PrecoProduto { get; set; }
+
+        [Required(ErrorMessage = "O campo Categoria é obrigatório.")]
+        public int? IdCategoria { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> ListaCategorias { get; set; }
+    }
+}
